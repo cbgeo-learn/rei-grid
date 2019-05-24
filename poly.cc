a8fc47df0249a37d0c2d901ec6a9d8b4a1942c7b
Rei Hosseini <reihos@utexas.edu>
2019-05-24 15:33:17 -0500

    Reading coordinates from file

--- poly.cc
+++ poly.cc
@@ -1,4 +1,5 @@
 #include <Eigen/Dense>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
@@ -8,15 +9,16 @@ protected:
   int n_ = coordlist_.size();
 
 public:
-  Poly(std::vector<Eigen::Vector2i> a) : coordlist_{a} {}
+  Poly(std::vector<Eigen::Vector2i> coordlist) : coordlist_{coordlist} {}
   virtual int area() = 0;
 };
 
 class Quadrilateral : public Poly {
 public:
-  Quadrilateral(std::vector<Eigen::Vector2i> a) : Poly(a) {}
+  Quadrilateral(std::vector<Eigen::Vector2i> coordlist) : Poly(coordlist) {}
   int area() {
-    int a = 0;
+    std::cout<<"calculating the area of the quadrilateral"<<std::endl;
+    int a= 0;
     for (int i = 0; i < n_; ++i) {
       int ipp = (i < n_ - 1) ? (i + 1) : 0;
       a += coordlist_[i](0) * coordlist_[ipp](1) -
@@ -29,8 +31,9 @@ public:
 
 class Triangle : public Poly {
 public:
-  Triangle(std::vector<Eigen::Vector2i> a) : Poly(a) {}
+  Triangle(std::vector<Eigen::Vector2i> coordlist) : Poly(coordlist) {}
   int area() {
+    std::cout<<"calculating the area of the triangle"<<std::endl;
     int a = 0;
     for (int i = 0; i < n_; ++i) {
       int ipp = (i < n_ - 1) ? (i + 1) : 0;
@@ -45,18 +48,26 @@ public:
 int main(int argc, char **argv) {
   std::vector<Eigen::Vector2i> coordlist;
   Eigen::Vector2i coords;
-  int n = (argc - 1) / 2;
-  if (argc % 2 == 0) {
+
+  std::ifstream coord("coordinates.txt");
+  std::vector<int> v;
+  int j;
+  while (coord >> j) {
+    v.push_back(j);
+  }
+  int n = v.size() / 2;
+ 
+  if (v.size() % 2 != 0) {
     std::cout << "Incorrect Number of Arguments!" << std::endl;
     std::abort();
   } else {
     for (int i = 0; i < n; ++i) {
-      coords(0) = atoi(argv[2 * i + 1]);
-      coords(1) = atoi(argv[2 * i + 2]);
+      coords(0) = v[2 * i];
+      coords(1) = v[2 * i + 1];
       coordlist.push_back(coords);
     }
   }
-  
+
   if (n == 4) {
     Quadrilateral shape(coordlist);
     std::cout << shape.area() << std::endl;
