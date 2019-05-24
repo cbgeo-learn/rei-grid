0c26a75f20c341549a2084de1b91889fad35139b
Rei Hosseini <reihos@utexas.edu>
2019-05-24 12:42:44 -0500

    Eigen:Vector used instead of std::array

--- poly.cc
+++ poly.cc
@@ -1,19 +1,19 @@
-#include <array>
 #include <iostream>
 #include <vector>
+#include<Eigen/Dense>
 
 class Poly {
 private:
-  std::vector<std::array<int, 2>> coordlist_;
+  std::vector<Eigen::Vector2i> coordlist_;
 
 public:
-  Poly(std::vector<std::array<int, 2>> a) : coordlist_{a} {}
+  Poly(std::vector<Eigen::Vector2i> a) : coordlist_{a} {}
   int area() {
     int a = 0;
     for (int i = 0; i < coordlist_.size(); ++i) {
       int ipp = (i < coordlist_.size() - 1) ? (i + 1) : 0;
-      a += coordlist_[i].at(0) * coordlist_[ipp].at(1) -
-           coordlist_[ipp].at(0) * coordlist_[i].at(1);
+      a += coordlist_[i](0) * coordlist_[ipp](1) -
+           coordlist_[ipp](0) * coordlist_[i](1);
     }
     a = a * 0.5;
     return a;
@@ -21,20 +21,21 @@ public:
 };
 
 int main(int argc, char **argv) {
-  std::vector<std::array<int, 2>> coordlist;
-
+  std::vector<Eigen::Vector2i> coordlist;
+  Eigen::Vector2i coords;
   if (argc % 2 == 0) {
     std::cout << "Incorrect Number of Arguments!" << std::endl;
     std::abort();
   } else {
     int n = (argc - 1) / 2;
     for (int i = 0; i < n; ++i) {
-      std::array<int, 2> coords = {atoi(argv[2 * i + 1]),
-                                   atoi(argv[2 * i + 2])};
+      coords(0)= atoi(argv[2 * i + 1]);
+      coords(1)=atoi(argv[2*i+2]);
       coordlist.push_back(coords);
     }
   }
 
   Poly poly(coordlist);
   std::cout << poly.area() << std::endl;
-}
+
+  }
