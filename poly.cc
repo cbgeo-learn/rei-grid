9d1f8ddaf332bb7e280beece13e781d8bae73fd2
Rei Hosseini <reihos@utexas.edu>
2019-05-24 14:28:39 -0500

    Derived classes added

--- poly.cc
+++ poly.cc
@@ -1,17 +1,39 @@
+#include <Eigen/Dense>
 #include <iostream>
 #include <vector>
-#include<Eigen/Dense>
 
 class Poly {
-private:
+protected:
   std::vector<Eigen::Vector2i> coordlist_;
+  int n_ = coordlist_.size();
 
 public:
   Poly(std::vector<Eigen::Vector2i> a) : coordlist_{a} {}
+  virtual int area() = 0;
+};
+
+class Quadrilateral : public Poly {
+public:
+  Quadrilateral(std::vector<Eigen::Vector2i> a) : Poly(a) {}
   int area() {
     int a = 0;
-    for (int i = 0; i < coordlist_.size(); ++i) {
-      int ipp = (i < coordlist_.size() - 1) ? (i + 1) : 0;
+    for (int i = 0; i < n_; ++i) {
+      int ipp = (i < n_ - 1) ? (i + 1) : 0;
+      a += coordlist_[i](0) * coordlist_[ipp](1) -
+           coordlist_[ipp](0) * coordlist_[i](1);
+    }
+    a = a * 0.5;
+    return a;
+  }
+};
+
+class Triangle : public Poly {
+public:
+  Triangle(std::vector<Eigen::Vector2i> a) : Poly(a) {}
+  int area() {
+    int a = 0;
+    for (int i = 0; i < n_; ++i) {
+      int ipp = (i < n_ - 1) ? (i + 1) : 0;
       a += coordlist_[i](0) * coordlist_[ipp](1) -
            coordlist_[ipp](0) * coordlist_[i](1);
     }
@@ -23,19 +45,26 @@ public:
 int main(int argc, char **argv) {
   std::vector<Eigen::Vector2i> coordlist;
   Eigen::Vector2i coords;
+  int n = (argc - 1) / 2;
   if (argc % 2 == 0) {
     std::cout << "Incorrect Number of Arguments!" << std::endl;
     std::abort();
   } else {
-    int n = (argc - 1) / 2;
     for (int i = 0; i < n; ++i) {
-      coords(0)= atoi(argv[2 * i + 1]);
-      coords(1)=atoi(argv[2*i+2]);
+      coords(0) = atoi(argv[2 * i + 1]);
+      coords(1) = atoi(argv[2 * i + 2]);
       coordlist.push_back(coords);
     }
   }
-
-  Poly poly(coordlist);
-  std::cout << poly.area() << std::endl;
-
+  
+  if (n == 4) {
+    Quadrilateral shape(coordlist);
+    std::cout << shape.area() << std::endl;
+  } else if (n == 3) {
+    Triangle shape(coordlist);
+    std::cout << shape.area() << std::endl;
+  } else {
+    std::cout << "Shape Not Allowed" << std::endl;
+    std::abort();
   }
+}
